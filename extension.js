7a603fa5e76d629efeb8c6711c74ed2ef098ac33
Michael Schrijer <m.schrijer@student.science.ru.nl>
2025-11-24 22:06:44 +0100

    Added shortcut and reading from model.xml file.

--- extension.js
+++ extension.js
@@ -2,6 +2,7 @@
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
 const xml2js = require('xml2js');
+const path = require('path');
 const fs = require('fs');
 const { exec } = require('child_process');
 // This method is called when your extension is activated
@@ -14,24 +15,27 @@ function activate(context) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "munge-feature-manager" is now active!');
+	console.log('Extension "munge-feature-manager" is now active!');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('munge-feature-manager.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
-
-		const xml = `<?xml version="1.0" encoding="UTF-8" standalone="no"?>
-		<configuration>
-			<feature automatic="selected" name="HelloWorld"/>
-			<feature automatic="selected" manual="selected" name="Feature"/>
-			<feature manual="selected" name="Wonderful"/>
-			<feature automatic="unselected" name="Beautiful"/>
-			<feature automatic="selected" manual="selected" name="World"/>
-		</configuration>`;
+	const disposable = vscode.commands.registerCommand('munge-feature-manager.compileWithMunge', function () {
 
 		const parser = new xml2js.Parser();
+		const currentDirectory = vscode.workspace.workspaceFolders[0].uri.fsPath
+		const xmlPath = path.join(currentDirectory, 'model.xml');
+		var xml;
+
+		try {
+			xml = fs.readFileSync(xmlPath, 'utf8');
+		} catch (error) {
+			if (error.code === 'ENOENT') {
+				vscode.window.showInformationMessage('model.xml is missing.');
+			} else {
+				vscode.window.showInformationMessage(error.message);
+			}
+		}
 
 		parser.parseStringPromise(xml)
 			.then(result => {
@@ -40,11 +44,10 @@ function activate(context) {
 				const featureNames = selectedFeatures.map(feature => feature.$.name);
 
 				if (featureNames.length > 0) {
-					const currentDirectory = vscode.workspace.workspaceFolders[0].uri.fsPath
 
 					fs.readdir(currentDirectory, (error, files) => {
 						if (error) {
-							console.error(`${error.message}`);
+							vscode.window.showInformationMessage(error.message);
 							return;
 						}
 
@@ -54,21 +57,20 @@ function activate(context) {
 
 							exec(command, (error, stdout, stderr) => {
 								if (error) {
-									console.error(`${error.message}`);
+									vscode.window.showInformationMessage(error.message);
 									return;
 								}
 								if (stderr) {
-									console.error(`${stderr}`);
+									vscode.window.showInformationMessage(error.message);
 									return;
 								}
-								console.log(`${stdout}`);
 							});
 						}
 					});
 				}
 			})
-			.catch(err => {
-				console.error(err);
+			.catch(error => {
+				vscode.window.showInformationMessage(error.message);
 			});
 	});
 
